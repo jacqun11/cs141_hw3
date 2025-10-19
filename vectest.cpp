8994223e3d221ca76c77f25f5c0e339745f4147e
Jacqueline Nguyen <jacqun11@uci.edu>
2025-10-18 21:11:24 -0700

    test (uncomment) using starter code in vectest

--- vectest.cpp
+++ vectest.cpp
@@ -3,13 +3,9 @@
 int main() // Here is a start:
 
 {
-    Vector<int> v1{1, 2, 3};
-    Vector<int> v2 = 20 * v1;
-    cout << v2 << endl;
-
-
-
-
+    // Vector<int> v1{1, 2, 3};
+    // Vector<int> v2 = 20 * v1;
+    // cout << v2 << endl;
 
     // cout << "v1 = " << v1 << endl;
     // cout << "v2 = " << v2 << endl;
@@ -24,33 +20,30 @@ int main() // Here is a start:
     // cout << "v2[4] = " << v2[4] << endl;
 
 
-//         Vector<int> intVec{1,3,5,7,9};
+        Vector<int> intVec{1,3,5,7,9};
 
-//         Vector<double> doubleVec{1.5,2.5,3.5,4.5};
+        Vector<double> doubleVec{1.5,2.5,3.5,4.5};
 
-//         Vector<int> iv{intVec};
+        Vector<int> iv{intVec};
 
-//         Vector<double> dv{doubleVec};
+        Vector<double> dv{doubleVec};
 
-//         cout << "intVec" << intVec << endl;
+        cout << "intVec" << intVec << endl;
 
-// // "intVec(1, 3, 5, 7, 9)"
+// "intVec(1, 3, 5, 7, 9)"
 
-//         cout << "iv" << iv << endl;
+        cout << "iv" << iv << endl;
 
-// // "iv(1, 3, 5, 7, 9)"
+// "iv(1, 3, 5, 7, 9)"
 
-//         cout << "doubleVec" << doubleVec << endl;
+        cout << "doubleVec" << doubleVec << endl;
 
-// // "doubleVec(1.5, 2.5, 3.5, 4.5)"
+// "doubleVec(1.5, 2.5, 3.5, 4.5)"
 
-// cout << "dv" << dv << endl;
-
-
-// // "dv(1.5, 2.5, 3.5, 4.5)"
+cout << "dv" << dv << endl;
 
 
-//         // add at least one test case for each method defined in Vector
+// "dv(1.5, 2.5, 3.5, 4.5)"
 
 return 0;
 
